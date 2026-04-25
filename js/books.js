c976f3d98f1142cd4cab8c3f7164251679265d47
CjMax <cjmaxdev@gmail.com>
2026-04-25 14:49:12 +0530

    Book JSON Array Fix

--- js/books.js
+++ js/books.js
@@ -1,35 +1,35 @@
 const BOOKS = [
-  {
-    id: 1,
-    title: "The Indian Stock Market Simplified",
-    desc: "Beginner-friendly guide to stock market concepts.",
-    price: 499,
-    image: "images/books/book1.webp",
-    status: "available"
-  },
-  {
-    id: 2,
-    title: "Trading in the Zone",
-    desc: "Advanced psychology for trading success.",
-    price: 999,
-    image: "images/books/book2.webp",
-    status: "available"
-  },
-  {
-    id: 3,
-    title: "Price Action Trading",
-    desc: "Learn real market setups and execution.",
-    price: 899,
-    image: "images/books/book3.webp",
-    status: "available"
-  },
-  {
-    id: 4,
-    title: "Price Action Trading",
-    desc: "Learn real market setups and execution.",
-    price: 899,
-    image: "images/books/book3.webp",
-    status: "available"
-  },
+  // {
+  //   id: 1,
+  //   title: "The Indian Stock Market Simplified",
+  //   desc: "Beginner-friendly guide to stock market concepts.",
+  //   price: 499,
+  //   image: "images/books/book1.webp",
+  //   status: "available"
+  // },
+  // {
+  //   id: 2,
+  //   title: "Trading in the Zone",
+  //   desc: "Advanced psychology for trading success.",
+  //   price: 999,
+  //   image: "images/books/book2.webp",
+  //   status: "available"
+  // },
+  // {
+  //   id: 3,
+  //   title: "Price Action Trading",
+  //   desc: "Learn real market setups and execution.",
+  //   price: 899,
+  //   image: "images/books/book3.webp",
+  //   status: "available"
+  // },
+  // {
+  //   id: 4,
+  //   title: "Price Action Trading",
+  //   desc: "Learn real market setups and execution.",
+  //   price: 899,
+  //   image: "images/books/book3.webp",
+  //   status: "available"
+  // },
   
 ];
